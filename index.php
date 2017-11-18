4f57cef2f6e78392aa6e567ebd4d666caf6504c9
8845musign <8845musign@gmail.com>
2017-11-19 01:32:13 +0900

    change top page layout

--- index.php
+++ index.php
@@ -4,15 +4,12 @@
 <body>
   <?php get_header(); ?>
 
-  <p>
-    このページは工事中です。腹筋しといてください。
-  </p>
-
-  <main>
+  <main class="top">
+    <h1 class="top__heading">最新記事</h1>
     <?php if (have_posts()) : ?>  
       <?php while (have_posts()) : the_post(); ?>
-        <article>
-          <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <article class="post-list">
+          <h2 class="post-list__items"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         </article>
       <?php endwhile; ?>
     <?php else : ?>
