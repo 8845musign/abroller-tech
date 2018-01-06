14e88d5c6e0c5bd0df1a292409653a12aa534e8a
8845musign <8845musign@gmail.com>
2018-01-06 17:08:14 +0900

    use wp function of loading style, for cache buster.

--- functions.php
+++ functions.php
@@ -5,3 +5,9 @@ function abroller_load_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'abroller_load_scripts' );
+
+function abroller_load_style() {
+	wp_enqueue_style( 'style', get_template_directory_uri().'/style.css', array(), '0.0.1', 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'abroller_load_style' );
--- head.php
+++ head.php
@@ -2,6 +2,5 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?><?php wp_title(' | '); ?></title>
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
   <?php wp_head(); ?>
 </head>
